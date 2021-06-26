bdf120d59b8ffc3bdeb42977ac14c639e4feb29e
dotnetfx <derekrowley@dotnetfx.com>
2021-06-26 10:25:36 -0600

    latest

--- example/src/App.js
+++ example/src/App.js
@@ -5,7 +5,7 @@ const App = () => {
   return (
     <React.Fragment>
       <Header2 fixed />
-      <Login1 onLogin={(value) => console.log(value)} passwordLength={8} forgot="#" remember={false} />
+      <Login1 onLogin={(value) => console.log(value)} passwordLength={8} forgotUrl="#" registerUrl="/auth/register" />
       <Footer1 className="bg-blue-100" />
     </React.Fragment>
   )
--- src/components/register1.js
+++ src/components/register1.js
@@ -1,16 +1,13 @@
 import React, { useState } from 'react'
 
-export default function Login1({ title, logo, remember, forgotUrl, registerUrl, onLogin, action, passwordLength, error, hidden }) {
+export default function Register1({ title, logo, loginUrl, onRegister, passwordLength }) {
 	const [email, setEmail] = useState(null)
 	const [password, setPassword] = useState(null)
-	const [rememberMe, setRememberMe] = useState(false)
 	const [errorMsg, setErrorMsg] = useState(error)
 
 	const _pwlen = passwordLength || 1
-	const _remember = remember == undefined ? true : remember
-	const _forgot = forgotUrl
-	const _register = registerUrl
-	const _title = title || "Sign Into Your Account"
+	const _loginUrl = loginUrl
+	const _title = title || "Register a New Account"
 	const _logo = logo || <img className="mx-auto h-12 w-auto" src="https://tailwindui.com/img/logos/workflow-mark-blue-600.svg" alt="Workflow" />
 
 	const isValid = () => {
@@ -33,8 +30,7 @@ export default function Login1({ title, logo, remember, forgotUrl, registerUrl, 
 					<div className="mx-auto">{_logo}</div>
 					<h2 className="mt-6 text-center text-3xl font-medium text-gray-900">{_title}</h2>
 				</div>
-				<form className="mt-8 space-y-6" action="#" method="POST" action={action}>
-					{hidden && hidden.map(h => (h))}
+				<form className="mt-8 space-y-6" action="#" method="POST">
 					<div className="rounded-md shadow-sm -space-y-px">
 						<div>
 							<label htmlFor="email-address" className="sr-only">Email address</label>
@@ -54,18 +50,6 @@ export default function Login1({ title, logo, remember, forgotUrl, registerUrl, 
 						</div>
 					</div>
 
-					{_remember &&
-						<div className="flex items-center">
-							<input id="remember_me" name="remember_me" type="checkbox" className="h-4 w-4 text-blue-600 focus:ring-blue-500 border-gray-300 rounded"
-								checked={rememberMe}
-								onChange={() => { setRememberMe(!rememberMe) }}
-							/>
-							<label htmlFor="remember_me" className="ml-2 block text-sm text-gray-900">
-								Remember me
-							</label>
-						</div>
-					}
-					
 					<div className="flex items-center justify-between">
 						{_forgot &&
 							<div className="text-sm">
@@ -74,11 +58,10 @@ export default function Login1({ title, logo, remember, forgotUrl, registerUrl, 
 								</a>
 							</div>
 						}
-
-						{_register &&
+						{_loginUrl &&
 							<div className="text-sm">
-								<a href={_register} className="font-medium text-blue-600 hover:text-blue-500">
-									Register for an account?
+								<a href={_loginUrl} className="font-medium text-blue-600 hover:text-blue-500">
+									Sign in instead?
 								</a>
 							</div>
 						}
@@ -88,7 +71,7 @@ export default function Login1({ title, logo, remember, forgotUrl, registerUrl, 
 					}
 
 					<div>
-						<button type={action ? "submit" : "button"} className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-blue-600 
+						<button type="button" className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-blue-600 
 						hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
 							onClick={() => { if (onLogin && isValid()) onLogin({ username: email, password, remember: rememberMe }) }}
 						>
