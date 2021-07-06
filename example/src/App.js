f7e95beabd7dcb9afcd580b817b86b6253f68dd5
dotnetfx <derekrowley@dotnetfx.com>
2021-07-06 15:06:21 -0600

    added blog1

--- example/src/App.js
+++ example/src/App.js
@@ -1,13 +1,22 @@
-import React from 'react'
-import { Header2, Footer1, Login1, Pricing1 } from 'react-tailblocks'
+import React, {useState} from 'react'
+import { Blog1, Header2, Footer1, Login1, Pricing1, Register1, Reset1, Modal1 } from 'react-tailblocks'
 
 const App = () => {
+
   return (
     <React.Fragment>
       <Header2 fixed />
+      <Blog1 />
+      <Modal1 />
       <Pricing1 />
+      <Register1 onRegister={(value) => console.log(value)} passwordLength={8} loginUrl="#" />
       <Login1 onLogin={(value) => console.log(value)} passwordLength={8} forgotUrl="#" registerUrl="/auth/register" />
-      <Footer1 className="bg-blue-100" />
+      <Reset1
+        loginUrl="#" 
+        onReset={(value) => {}}
+      />
+
+      <Footer1 />
     </React.Fragment>
   )
 }
