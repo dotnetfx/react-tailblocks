c596b29b361784bd6abf8e1f1df435a12abaf9be
dotnetfx <derekrowley@dotnetfx.com>
2021-07-09 10:33:58 -0600

    tweaks to layout1

--- src/components/layout1.js
+++ src/components/layout1.js
@@ -14,49 +14,51 @@ export default function Form1({ className, panels }) {
             </p>,
         content:
             <Form onSubmit={(data) => { console.log(data) }}>
-                <div className="grid grid-cols-3 gap-6">
-                    <div className="col-span-3 sm:col-span-2">
-                        <Input name="website" label="Website" prefix="http://" placeHolder="www.example.com" />
+                <div className="px-4 sm:p-6">
+                    <div className="grid grid-cols-3 gap-6">
+                        <div className="col-span-3 sm:col-span-2">
+                            <Input name="website" label="Website" prefix="http://" placeHolder="www.example.com" />
+                        </div>
                     </div>
-                </div>
 
-                <TextArea name="about" label="About" placeHolder="All about you" description="Brief description for your profile. URLs are hyperlinked." />
+                    <TextArea name="about" label="About" placeHolder="All about you" description="Brief description for your profile. URLs are hyperlinked." />
 
-                <div>
-                    <label className="block text-sm font-medium text-gray-700">
-                        Photo
-                    </label>
-                    <div className="mt-1 flex items-center">
-                        <span className="inline-block h-12 w-12 rounded-full overflow-hidden bg-gray-100">
-                            <UserCircleIcon className="h-full w-full text-gray-300" />
-                        </span>
-                        <button type="button" className="ml-5 bg-white py-2 px-3 border border-gray-300 rounded-md shadow-sm text-sm leading-4 font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
-                            Change
-                        </button>
+                    <div>
+                        <label className="block text-sm font-medium text-gray-700">
+                            Photo
+                        </label>
+                        <div className="mt-1 flex items-center">
+                            <span className="inline-block h-12 w-12 rounded-full overflow-hidden bg-gray-100">
+                                <UserCircleIcon className="h-full w-full text-gray-300" />
+                            </span>
+                            <button type="button" className="ml-5 bg-white py-2 px-3 border border-gray-300 rounded-md shadow-sm text-sm leading-4 font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
+                                Change
+                            </button>
+                        </div>
                     </div>
-                </div>
 
-                <div>
-                    <label className="block text-sm font-medium text-gray-700">
-                        Cover photo
-                    </label>
-                    <div className="mt-1 flex justify-center px-6 pt-5 pb-6 border-2 border-gray-300 border-dashed rounded-md">
-                        <div className="space-y-1 text-center">
-                            <PhotographIcon className="mx-auto h-12 w-12 text-gray-400" />
-                            <div className="flex text-sm text-gray-600">
-                                <label htmlFor="file-upload" className="relative cursor-pointer bg-white rounded-md font-medium text-blue-600 hover:text-blue-500 focus-within:outline-none focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-blue-500">
-                                    <span>Upload a file</span>
-                                    <input id="file-upload" name="file-upload" type="file" className="sr-only" />
-                                </label>
-                                <p className="pl-1">or drag and drop</p>
+                    <div>
+                        <label className="block text-sm font-medium text-gray-700">
+                            Cover photo
+                        </label>
+                        <div className="mt-1 flex justify-center px-6 pt-5 pb-6 border-2 border-gray-300 border-dashed rounded-md">
+                            <div className="space-y-1 text-center">
+                                <PhotographIcon className="mx-auto h-12 w-12 text-gray-400" />
+                                <div className="flex text-sm text-gray-600">
+                                    <label htmlFor="file-upload" className="relative cursor-pointer bg-white rounded-md font-medium text-blue-600 hover:text-blue-500 focus-within:outline-none focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-blue-500">
+                                        <span>Upload a file</span>
+                                        <input id="file-upload" name="file-upload" type="file" className="sr-only" />
+                                    </label>
+                                    <p className="pl-1">or drag and drop</p>
+                                </div>
+                                <p className="text-xs text-gray-500">
+                                    PNG, JPG, GIF up to 10MB
+                                </p>
                             </div>
-                            <p className="text-xs text-gray-500">
-                                PNG, JPG, GIF up to 10MB
-                            </p>
                         </div>
                     </div>
                 </div>
-                <div className="px-4 py-3 bg-gray-50 text-right sm:px-6">
+                <div className="px-4 p-3 bg-gray-50 text-right sm:px-6 mt-8">
                     <button type="submit" className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
                         Save
                     </button>
@@ -70,7 +72,7 @@ export default function Form1({ className, panels }) {
             </p>,
         content:
             <Form onSubmit={(data) => { console.log(data) }}>
-                <div className="grid grid-cols-6 gap-6">
+                <div className="grid grid-cols-6 gap-6 px-4 sm:p-6">
                     <div className="col-span-6 sm:col-span-3">
                         <Input name="firstName" label="First name" />
                     </div>
@@ -113,6 +115,7 @@ export default function Form1({ className, panels }) {
                     </button>
                 </div>
             </Form>
+
     }, {
         title: <h3 className="text-lg font-medium leading-6 text-gray-900">Notifications</h3>,
         description:
@@ -121,25 +124,27 @@ export default function Form1({ className, panels }) {
             </p>,
         content:
             <Form onSubmit={(data) => { console.log(data) }}>
-                <fieldset>
-                    <legend className="text-base font-medium text-gray-900">By Email</legend>
-                    <div className="mt-4 space-y-4">
-                        <Input name="comments" type="checkbox" label="Comments" description="Get notified when someone posts a comment." />
-                        <Input name="candidates" type="checkbox" label="Candidates" description="Get notified when a candidate applies for a job." />
-                        <Input name="offers" type="checkbox" label="Offers" description="et notified when a candidate accepts or rejects an offer." />
-                    </div>
-                </fieldset>
-                <fieldset>
-                    <div>
-                        <legend className="text-base font-medium text-gray-900">Push Notifications</legend>
-                        <p className="text-sm text-gray-500">These are delivered via SMS to your mobile phone.</p>
-                    </div>
-                    <div className="mt-4 space-y-4">
-                        <Input name="pushEverything" type="radio" label="Everything" />
-                        <Input name="pushEmail" type="radio" label="Same as email" />
-                        <Input name="pushNothing" type="radio" label="No push notifications" />
-                    </div>
-                </fieldset>
+                <div className="px-4 sm:p-6">
+                    <fieldset>
+                        <legend className="text-base font-medium text-gray-900">By Email</legend>
+                        <div className="mt-4 space-y-4">
+                            <Input name="comments" type="checkbox" label="Comments" description="Get notified when someone posts a comment." />
+                            <Input name="candidates" type="checkbox" label="Candidates" description="Get notified when a candidate applies for a job." />
+                            <Input name="offers" type="checkbox" label="Offers" description="et notified when a candidate accepts or rejects an offer." />
+                        </div>
+                    </fieldset>
+                    <fieldset>
+                        <div>
+                            <legend className="text-base font-medium text-gray-900">Push Notifications</legend>
+                            <p className="text-sm text-gray-500">These are delivered via SMS to your mobile phone.</p>
+                        </div>
+                        <div className="mt-4 space-y-4">
+                            <Input name="pushEverything" type="radio" label="Everything" />
+                            <Input name="pushEmail" type="radio" label="Same as email" />
+                            <Input name="pushNothing" type="radio" label="No push notifications" />
+                        </div>
+                    </fieldset>
+                </div>
                 <div className="px-4 py-3 bg-gray-50 text-right sm:px-6">
                     <button type="submit" className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
                         Save
@@ -168,7 +173,7 @@ export default function Form1({ className, panels }) {
                             </div>
                             <div className="mt-5 md:mt-0 md:col-span-2">
                                 <div className="shadow overflow-hidden sm:rounded-md">
-                                    <div className="px-4 py-5 bg-white space-y-6 sm:p-6">
+                                    <div className="pt-5 bg-white space-y-6">
                                         {p.content}
                                     </div>
                                 </div>
